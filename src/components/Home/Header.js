b0aecaf3079c6805a950b16b227576a13d44d9a9
Simon Weizman <simon@weizman.us>
2020-11-11 22:58:08 +0000

    Update Header.js

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
-import Button from "react-bootstrap/Button";
 
 
 function Header() {
