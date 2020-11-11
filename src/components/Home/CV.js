65c16631833ef4f2ec60c932f222d431be31a22b
Simon Weizman <simon@weizman.us>
2020-11-11 22:57:48 +0000

    Update CV.js

--- src/components/Home/CV.js
+++ src/components/Home/CV.js
@@ -2,7 +2,6 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
 import CardDeck from "react-bootstrap/CardDeck";
 import Card from "react-bootstrap/Card";
 
