6223a5ea38890a06306e58c1041512ecac29abc3
Simon Weizman <simon@weizman.us>
2020-11-13 19:54:36 -0500

    Added aria labels to Footer social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,13 +12,13 @@ function Footer() {
 		<Row className="item justify-content-center">
 		</Row>
         <Row className="item social footer-socials justify-content-center">
-			<a href="https://www.linkedin.com/in/simon-weizman"><i class="fab fa-linkedin-in"></i></a>
-			<a href="https://www.facebook.com/simon.weizman.13"><i class="fab fa-facebook-f"></i></a>
-			<a href="https://twitter.com/torrentofshame"><i class="fab fa-twitter"></i></a>
-			<a href="https://www.instagram.com/mviwxg/"><i class="fab fa-instagram"></i></a>
-			<a href="https://gitlab.com/torrentofshame"><i class="fab fa-gitlab"></i></a>
-			<a href="https://github.com/Torrentisthegreatest"><i class="fab fa-github"></i></a>
-			<a href="https://discord.gg/MYvd3RB"><i class="fab fa-discord"></i></a>
+			<a aria-label="Linkedin" href="https://www.linkedin.com/in/simon-weizman"><i aria-hidden="true" className="fab fa-linkedin-in"></i></a>
+			<a aria-label="Facebook" href="https://www.facebook.com/simon.weizman.13"><i aria-hidden="true" className="fab fa-facebook-f"></i></a>
+			<a aria-label="Twitter" href="https://twitter.com/torrentofshame"><i aria-hidden="true" className="fab fa-twitter"></i></a>
+			<a aria-label="Instagram" href="https://www.instagram.com/mviwxg/"><i aria-hidden="true" className="fab fa-instagram"></i></a>
+			<a aria-label="GitLab" href="https://gitlab.com/torrentofshame"><i aria-hidden="true" className="fab fa-gitlab"></i></a>
+			<a aria-label="GitHub" href="https://github.com/Torrentisthegreatest"><i aria-hidden="true" className="fab fa-github"></i></a>
+			<a aria-label="Discord" href="https://discord.gg/MYvd3RB"><i aria-hidden="true" className="fab fa-discord"></i></a>
         </Row>
 		<Row className="item justify-content-center mt-3">
 			<p className="copyright">© 2020 - Simon Weizman</p>
