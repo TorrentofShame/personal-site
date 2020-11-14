a75f938a531ed4f0db4abdbd66c234a13463b49c
Simon Weizman <simon@weizman.us>
2020-11-13 20:21:34 -0500

    Fixed Footer to fit WCAG Standards

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
@@ -12,13 +13,13 @@ function Footer() {
 		<Row className="item justify-content-center">
 		</Row>
         <Row className="item social footer-socials justify-content-center">
-			<a aria-label="Linkedin" href="https://www.linkedin.com/in/simon-weizman"><i aria-hidden="true" className="fab fa-linkedin-in"></i></a>
-			<a aria-label="Facebook" href="https://www.facebook.com/simon.weizman.13"><i aria-hidden="true" className="fab fa-facebook-f"></i></a>
-			<a aria-label="Twitter" href="https://twitter.com/torrentofshame"><i aria-hidden="true" className="fab fa-twitter"></i></a>
-			<a aria-label="Instagram" href="https://www.instagram.com/mviwxg/"><i aria-hidden="true" className="fab fa-instagram"></i></a>
-			<a aria-label="GitLab" href="https://gitlab.com/torrentofshame"><i aria-hidden="true" className="fab fa-gitlab"></i></a>
-			<a aria-label="GitHub" href="https://github.com/Torrentisthegreatest"><i aria-hidden="true" className="fab fa-github"></i></a>
-			<a aria-label="Discord" href="https://discord.gg/MYvd3RB"><i aria-hidden="true" className="fab fa-discord"></i></a>
+			<Link aria-label="Linkedin" to="https://www.linkedin.com/in/simon-weizman"><i aria-hidden="true" className="fab fa-linkedin-in" /></Link>
+			<Link aria-label="Facebook" to="https://www.facebook.com/simon.weizman.13"><i aria-hidden="true" className="fab fa-facebook-f" /></Link>
+			<Link aria-label="Twitter" to="https://twitter.com/torrentofshame"><i aria-hidden="true" className="fab fa-twitter" /></Link>
+			<Link aria-label="Instagram" to="https://www.instagram.com/mviwxg/"><i aria-hidden="true" className="fab fa-instagram" /></Link>
+			<Link aria-label="GitLab" to="https://gitlab.com/torrentofshame"><i aria-hidden="true" className="fab fa-gitlab" /></Link>
+			<Link aria-label="GitHub" to="https://github.com/Torrentisthegreatest"><i aria-hidden="true" className="fab fa-github" /></Link>
+			<Link aria-label="Discord" to="https://discord.gg/MYvd3RB"><i aria-hidden="true" className="fab fa-discord" /></Link>
         </Row>
 		<Row className="item justify-content-center mt-3">
 			<p className="copyright">© 2020 - Simon Weizman</p>
