3363496491f6c51bdb09b6b0eb3e02e90b3c98af
Simon Weizman <simon@weizman.us>
2021-03-09 11:18:14 -0500

    Fixed issue with social icons in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
-import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
@@ -12,13 +12,13 @@ function Footer() {
         <Row className="item justify-content-center">
         </Row>
         <Row className="item social footer-socials justify-content-center">
-          <Link aria-label="Linkedin" to="https://www.linkedin.com/in/simon-weizman"><i aria-hidden="true" className="fab fa-linkedin-in" /></Link>
-          <Link aria-label="Facebook" to="https://www.facebook.com/simon.weizman.13"><i aria-hidden="true" className="fab fa-facebook-f" /></Link>
-          <Link aria-label="Twitter" to="https://twitter.com/torrentofshame"><i aria-hidden="true" className="fab fa-twitter" /></Link>
-          <Link aria-label="Instagram" to="https://www.instagram.com/mviwxg/"><i aria-hidden="true" className="fab fa-instagram" /></Link>
-          <Link aria-label="GitLab" to="https://gitlab.com/torrentofshame"><i aria-hidden="true" className="fab fa-gitlab" /></Link>
-          <Link aria-label="GitHub" to="https://github.com/Torrentisthegreatest"><i aria-hidden="true" className="fab fa-github" /></Link>
-          <Link aria-label="Discord" to="https://discord.gg/MYvd3RB"><i aria-hidden="true" className="fab fa-discord" /></Link>
+          <a target="_blank" rel="noopener noreferrer" aria-label="Linkedin" href="https://www.linkedin.com/in/simonweizman"><i aria-hidden="true" className="fab fa-linkedin-in" /></a>
+          <a target="_blank" rel="noopener noreferrer" aria-label="Facebook" href="https://www.facebook.com/simon.weizman.13"><i aria-hidden="true" className="fab fa-facebook-f" /></a>
+          <a target="_blank" rel="noopener noreferrer" aria-label="Twitter" href="https://twitter.com/torrentofshame"><i aria-hidden="true" className="fab fa-twitter" /></a>
+          <a target="_blank" rel="noopener noreferrer" aria-label="Instagram" href="https://www.instagram.com/mviwxg/"><i aria-hidden="true" className="fab fa-instagram" /></a>
+          <a target="_blank" rel="noopener noreferrer" aria-label="GitLab" href="https://gitlab.com/torrentofshame"><i aria-hidden="true" className="fab fa-gitlab" /></a>
+          <a target="_blank" rel="noopener noreferrer" aria-label="GitHub" href="https://github.com/TorrentofShame"><i aria-hidden="true" className="fab fa-github" /></a>
+          <a title="@torrent#0662" target="_blank" rel="noopener noreferrer" aria-label="Discord" href="https://discord.gg/MYvd3RB"><i aria-hidden="true" className="fab fa-discord" /></a>
         </Row>
         <Row className="item justify-content-center mt-3">
           <p className="copyright">© 2020 - Simon Weizman</p>
